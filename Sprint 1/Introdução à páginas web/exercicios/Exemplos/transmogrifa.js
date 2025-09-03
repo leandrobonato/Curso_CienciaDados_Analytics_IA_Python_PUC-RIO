6c3d81b1f8e9df1bd6e44901ec73d48a503cb777
Leandro Miozzo Bonato <leandro-bonato@hotmail.com.br>
2025-09-02 23:10:25 -0300

    Criação do arquivo de dúvidas de introdução à banco de dados. 2. criiação de uma pasta de exemplos de html e javasscript e css. 3. criação do workspac

---
+++ Sprint 1/Introdução à páginas web/exercicios/Exemplos/transmogrifa.js
@@ -0,0 +1,21 @@
+inicio();
+
+function transmogrifa(a, b){
+    {
+        var x
+        let y;
+        {
+            x = a + b;
+            y = a * b;
+        }        
+    }
+    return x / y;
+}
+
+function inicio(){
+    let x = 8;
+    let y = 7;
+
+    let z = transmogrifa(x, y);
+    console.log(z);
+}
